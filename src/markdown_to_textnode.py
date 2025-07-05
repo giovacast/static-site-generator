547d5ea29409a8a2b10c2f61d645db3b2edd2624
giovacast <giovacastiglione@gmail.com>
2025-07-05 13:12:29 -0400

    feat: Added image and link extraction functions to this module. This commit introduces two new functions added to this module. One function extracts images from TextNodes with markdown text and returns a new list of TextNodes in which markdown images and text have been separated into their own TextNodes. The second function extracts links from a list of TextNodes and returns a new list of Textnodes in which markdown links and text have been separated into their own TextNodes. Each of these functions use the helper functions contained in the extract_from_mkdwn.py module.

--- src/markdown_to_textnode.py
+++ src/markdown_to_textnode.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from extract_from_mkdwn import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -23,5 +24,69 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
 
-    
+    for old_node in old_nodes:
+        if not isinstance(old_node, TextNode):
+            raise TypeError("TypeError: node needs to be a TextNode.")
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        image = extract_markdown_images(old_node.text)
+        if not image:
+             new_nodes.append(old_node)
+             continue
+        
+        temp_nodes = []
+        remaining_text = old_node.text
+        for image_alt, image_link in image:
+            # Split on current image
+            parts = remaining_text.split(f"![{image_alt}]({image_link})", 1)
+            if parts[0]:
+                temp_nodes.append(TextNode(parts[0], TextType.TEXT))
+            # Add image node
+            temp_nodes.append(TextNode(image_alt, TextType.IMAGE, 
+                                               image_link))
+            # Update remaining text
+            remaining_text = parts[1]
+            # Add final text after last image
+        if remaining_text:
+                temp_nodes.append(TextNode(remaining_text, TextType.TEXT))
+
+        new_nodes.extend(temp_nodes)
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if not isinstance(old_node, TextNode):
+            raise TypeError("TypeError: node needs to be a TextNode.")
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        link = extract_markdown_links(old_node.text)
+        if not link:
+            new_nodes.append(old_node)
+            continue
+
+        temp_nodes = []
+        remaining_text = old_node.text
+        for anchor_txt, url in link:
+            # Split on current link
+            parts = remaining_text.split(f"[{anchor_txt}]({url})", 1)
+            if parts[0]:
+                temp_nodes.append(TextNode(parts[0], TextType.TEXT))
+            # Add link node    
+            temp_nodes.append(TextNode(anchor_txt, TextType.LINK, url))
+            # Update remaining text
+            remaining_text = parts[1]
+        # Add final text after last link
+        if remaining_text:
+            temp_nodes.append(TextNode(remaining_text, TextType.TEXT))
+
+        new_nodes.extend(temp_nodes)
+    return new_nodes
