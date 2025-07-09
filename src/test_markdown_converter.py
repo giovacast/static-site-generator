8647083091bc0124cbeb1dfa5bb34d787025f29e
giovacast <giovacastiglione@gmail.com>
2025-07-09 11:57:11 -0400

    test: Added a test for the text to textnode module. This commit introduces a single test to check that the function can parse through a piece of md text and return a list of TextNodes separated by different types of inline text and in the same order they appeared in the original text. There's no need to test for edge cases or errors since the helper functions used by this module already check for those.

--- src/test_markdown_converter.py
+++ src/test_markdown_converter.py
@@ -2,6 +2,7 @@ import unittest
 from textnode import TextType, TextNode
 from markdown_to_textnode import split_nodes_delimiter, split_nodes_image
 from markdown_to_textnode import split_nodes_link
+from text_to_textnodes import text_to_textnodes
 
 class TestMarkdowntoTextNode(unittest.TestCase):
 
@@ -214,4 +215,28 @@ class TestExtractLinksFromMarkdown(unittest.TestCase):
                 TextNode("This is only text with no links.", TextType.TEXT)
             ],
             new_nodes
+        )
+
+class TestTextToTextNodes(unittest.TestCase):
+
+    def test_text_to_textnodes(self):
+
+        nodes = text_to_textnodes("This is **text** with an _italic_ word and a `code block` " \
+"and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) " \
+"and a [link](https://boot.dev)")
+        self.assertListEqual(
+            [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, 
+                     "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev")
+            ]
+            , nodes
         )
