b856277396f87998f23c2fa65979e1ef2ac80db6
giovacast <giovacastiglione@gmail.com>
2025-08-03 08:57:03 -0400

    Add copy_to_directory and copy_directory_contents functions to main.py module.
    copy_to_directory takes a source and destination directories and copies the contents in the source directory to the destination directory.
    
    First, it prepares the destination directory by deleting it and recreating it, we want to make sure it's a clean copy and only the elements in the source directory are in the destination by the time the function finishes running.
    
    We then call the copy_directory_contents helper function recursively to copy all files and sub-directories in the source to the destination.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,42 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_directory_contents(source, destination):
+    """Copies all files and subdirectories from within a
+    directory to the main destination directory."""
+    source_dir = os.listdir(source)
+    for item in source_dir:
+        full_source_path = os.path.join(source, item)
+        full_destination_path = os.path.join(destination, item)
+        if os.path.isfile(full_source_path):
+            shutil.copy(full_source_path, full_destination_path)
+            print(f"Copying file \"{item}\" from {full_source_path} to {full_destination_path}")
+        else:
+            os.mkdir(full_destination_path, mode=0o755)
+            copy_directory_contents(full_source_path, full_destination_path)
+
+def copy_to_directory(source, destination):
+    """Copies the entire contents of the source directory
+    to a destination directory."""
+    # Delete all contents in the destination directory
+    # 1. Use the destination address, check if it exists
+    # 2. Delete every directory and file in it
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+        os.mkdir(destination, mode=0o755)
+    else:
+        os.mkdir(destination, mode=0o755)
+
+    copy_directory_contents(source, destination)
 
 def main():
     # Create a TextNode object with dummy values
     node = TextNode("Sample text", TextType.LINK, "https://example.com")
     # Print the object using repr to show its details
+    source = "/Users/giovannicastiglione/workspace/github.com/giovacast/static-site-generator/static"
+    destination = "/Users/giovannicastiglione/workspace/github.com/giovacast/static-site-generator/public"
+    copy_to_directory(source, destination)
     print(node.__repr__())
 
 if __name__ == "__main__":
